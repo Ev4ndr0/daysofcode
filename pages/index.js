b6e3db63bd53fae19e40be5ca9ac4d2e061071f5
Evandro Macedo <evandro.macedo@acnsf.com.br>
2022-04-22 19:22:34 -0300

    Atualizado index com os componentes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
-import styled from 'styled-components'
+import Head from "../src/components/Head";
+
+import styled from "styled-components";
+import Menu from "../src/components/Menu";
+import AssinaturaNewsletter from "../src/components/Assinatura";
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
 
-export default function Home() {
-  return <Title>My page</Title>
+export default function HomePage() {
+  return (
+    <main>
+      <Head title="Home - Site" />      
+        <Menu />
+        <AssinaturaNewsletter />
+      
+    </main>
+  );
 }
